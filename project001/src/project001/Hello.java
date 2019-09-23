8bec97596668b365620a1fa0a31d2217060a0dfd
LeeMyungJic <remge007@naver.com>
2019-09-23 21:08:35 +0900

    project001
    숫자와 문자열

---
+++ project001/src/project001/Hello.java
@@ -0,0 +1,16 @@
+package project001;
+
+public class Hello {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("명직");
+        System.out.println("명직" + "입니다");
+        System.out.println(1+2);
+        System.out.println("12" + "34");
+        System.out.println('1' + '2');
+        System.out.println("저는 \"명직\" 입니다");
+        System.out.println("저는\n명직\n입니다");
+    }
+}
