ca94beb1a95174341462125eb84717058c17fb70
LeeMyungJic <remge007@naver.com>
2019-09-26 22:52:42 +0900

    Re

---
+++ project002 - Variable/src/variable/Variable.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a;
+        a = 1;
+        System.out.println(a+1);
+        a = 2;
+        System.out.println(a+1);
+    }
+}
